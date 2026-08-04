17272860007280115656b385f7ab5cb511a0cd3f
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-04 20:39:55 +0600

    Update childController.js

--- App/controllers/childController.js
+++ App/controllers/childController.js
@@ -1,4 +1,10 @@
 const userModel = require('../models/userModel');
 const childModel = require('../models/childModel');
 
+exports.index = (req, res) => {
+  const user = userModel.current();
+  const child = childModel.forUser(user.id);
+  const growth = childModel.growth(child.id);
+  const milestones = childModel.milestones(child.id);
+  const achieved = milestones.filter(m => m.achieved).length;
 
