29f926d48914ff5f8af6e13d5222c61b8ee82542
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-04 20:38:27 +0600

    Update appointmentController.js

--- App/controllers/appointmentController.js
+++ App/controllers/appointmentController.js
@@ -1,3 +1,8 @@
 const userModel = require('../models/userModel');
 const appointmentModel = require('../models/appointmentModel');
 
+exports.index = (req, res) => {
+  const user = userModel.current();
+  const specialty = req.query.specialty || 'All';
+  const availableOnly = req.query.available === '1';
+  res.render('appointments', {
