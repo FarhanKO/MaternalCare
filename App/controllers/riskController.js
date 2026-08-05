7f0fca0fdb536243a78cfc1b52dafab9c18778da
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 20:03:33 +0600

    Update riskController.js

--- App/controllers/riskController.js
+++ App/controllers/riskController.js
@@ -1,1 +1,5 @@
+const userModel = require('../models/userModel');
+const pregnancyModel = require('../models/pregnancyModel');
+const vitalModel = require('../models/vitalModel');
+const riskModel = require('../models/riskModel');
 
