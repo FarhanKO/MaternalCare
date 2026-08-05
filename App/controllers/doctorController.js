e12ea3f9945de0e54f6dcdb25f676e468c032c5b
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 20:05:24 +0600

    Update doctorController.js

--- App/controllers/doctorController.js
+++ App/controllers/doctorController.js
@@ -4,3 +4,17 @@ const vitalModel = require('../models/vitalModel');
 const riskModel = require('../models/riskModel');
 const appointmentModel = require('../models/appointmentModel');
 
+exports.index = (req, res) => {
+  const patient = userModel.current();
+  const pregnancy = pregnancyModel.forUser(patient.id);
+  res.render('doctor', {
+    page: 'doctor',
+    user: patient,
+    patient, pregnancy,
+    vitals: vitalModel.history(patient.id),
+    latest: vitalModel.latest(patient.id),
+    alerts: vitalModel.alerts(patient.id),
+    risk: riskModel.fromLatestVitals(patient, pregnancy),
+    appointments: appointmentModel.forUser(patient.id).filter(a => a.status !== 'cancelled'),
+  });
+};
