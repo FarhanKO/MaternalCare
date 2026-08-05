c7ab91ce5f4000befe87811bb36e368be7a52da8
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 20:04:37 +0600

    Update learnController.js

--- App/controllers/learnController.js
+++ App/controllers/learnController.js
@@ -1,3 +1,15 @@
 const userModel = require('../models/userModel');
 const contentModel = require('../models/contentModel');
 
+exports.index = (req, res) => {
+  const user = userModel.current();
+  const category = req.query.category || 'All';
+  res.render('learn', {
+    page: 'learn', user, category,
+    articles: contentModel.articles(category),
+    categories: contentModel.articleCategories(),
+    posts: contentModel.posts(),
+    posted: req.query.posted === '1',
+  });
+};
+
