b613c1b3fe81f0981b5ceb968986ebb50c8908d4
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 20:03:41 +0600

    Update riskController.js

--- App/controllers/riskController.js
+++ App/controllers/riskController.js
@@ -3,3 +3,14 @@ const pregnancyModel = require('../models/pregnancyModel');
 const vitalModel = require('../models/vitalModel');
 const riskModel = require('../models/riskModel');
 
+exports.index = (req, res) => {
+  const user = userModel.current();
+  const pregnancy = pregnancyModel.forUser(user.id);
+  const latest = vitalModel.latest(user.id);
+  res.render('risk', {
+    page: 'risk', user, pregnancy, latest,
+    result: riskModel.fromLatestVitals(user, pregnancy),
+    custom: false,
+  });
+};
+
