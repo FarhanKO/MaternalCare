12e2668a46eab1110ba7e913d689e7a0b021f3f0
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 20:04:09 +0600

    Update pregnancyController.js

--- App/controllers/pregnancyController.js
+++ App/controllers/pregnancyController.js
@@ -2,3 +2,12 @@ const userModel = require('../models/userModel');
 const pregnancyModel = require('../models/pregnancyModel');
 const vitalModel = require('../models/vitalModel');
 
+exports.index = (req, res) => {
+  const user = userModel.current();
+  const pregnancy = pregnancyModel.forUser(user.id);
+  res.render('pregnancy', {
+    page: 'pregnancy', user, pregnancy,
+    timeline: pregnancyModel.timeline(pregnancy.week),
+    vitals: vitalModel.history(user.id),
+  });
+};
