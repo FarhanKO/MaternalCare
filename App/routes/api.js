bd769e3d8d54f77a3204cc890f9a147f9a3add11
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 22:59:43 +0600

    Update api.js

--- App/routes/api.js
+++ App/routes/api.js
@@ -10,3 +10,10 @@ const reminderApi = require('../controllers/api/reminderApiController');
 const userModel = require('../models/userModel');
 const pregnancyModel = require('../models/pregnancyModel');
 
+/* current demo session user + pregnancy summary */
+router.get('/me', (req, res) => {
+  const user = userModel.current();
+  const pregnancy = pregnancyModel.forUser(user.id);
+  res.json({ data: { user, pregnancy } });
+});
+
