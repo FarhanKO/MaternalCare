5af5bf37582270a3b01702ca03036ff10d1394c3
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 22:58:53 +0600

    Update web.js

--- App/routes/web.js
+++ App/routes/web.js
@@ -12,3 +12,15 @@ const emergencyController = require('../controllers/emergencyController');
 const learnController = require('../controllers/learnController');
 const doctorController = require('../controllers/doctorController');
 
+router.get('/', dashboardController.home);
+router.get('/dashboard', dashboardController.dashboard);
+
+router.get('/pregnancy', pregnancyController.index);
+
+router.get('/vitals', vitalsController.index);
+router.post('/vitals', vitalsController.create);
+
+router.get('/child', childController.index);
+router.post('/child/growth', childController.addGrowth);
+router.post('/child/milestone/:id/toggle', childController.toggleMilestone);
+
