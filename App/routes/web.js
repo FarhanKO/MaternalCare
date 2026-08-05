134422bb2a62b5c176e2959c4c457f29825a4083
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 22:58:43 +0600

    Update web.js

--- App/routes/web.js
+++ App/routes/web.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const dashboardController = require('../controllers/dashboardController');
+const pregnancyController = require('../controllers/pregnancyController');
+const vitalsController = require('../controllers/vitalsController');
+const childController = require('../controllers/childController');
+const vaccinationController = require('../controllers/vaccinationController');
+const appointmentController = require('../controllers/appointmentController');
+const riskController = require('../controllers/riskController');
+const emergencyController = require('../controllers/emergencyController');
+const learnController = require('../controllers/learnController');
+const doctorController = require('../controllers/doctorController');
+
