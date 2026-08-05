f246cbda09d6d4012535ea8239de270a42b983b9
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 20:03:47 +0600

    Update riskController.js

--- App/controllers/riskController.js
+++ App/controllers/riskController.js
@@ -14,3 +14,17 @@ exports.index = (req, res) => {
   });
 };
 
+exports.assess = (req, res) => {
+  const user = userModel.current();
+  const pregnancy = pregnancyModel.forUser(user.id);
+  const { age, systolic, diastolic, sugar, temp } = req.body;
+  const result = riskModel.assess({
+    age: Number(age), systolic: Number(systolic), diastolic: Number(diastolic),
+    sugar: Number(sugar), temp: Number(temp), week: pregnancy.week,
+  });
+  res.render('risk', {
+    page: 'risk', user, pregnancy,
+    latest: { systolic, diastolic, sugar, temp_c: temp },
+    result, custom: true,
+  });
+};
