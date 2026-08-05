ffbfdee450888f992519ef729d54a4b5a9dc2c41
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 20:05:17 +0600

    Update doctorController.js

--- App/controllers/doctorController.js
+++ App/controllers/doctorController.js
@@ -1,1 +1,6 @@
+const userModel = require('../models/userModel');
+const pregnancyModel = require('../models/pregnancyModel');
+const vitalModel = require('../models/vitalModel');
+const riskModel = require('../models/riskModel');
+const appointmentModel = require('../models/appointmentModel');
 
