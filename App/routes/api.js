957b66ced9eb71088b93fa371b3038e5e8d39b81
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 22:59:33 +0600

    Update api.js

--- App/routes/api.js
+++ App/routes/api.js
@@ -5,4 +5,8 @@
 const express = require('express');
 const router = express.Router();
 
+const symptomApi = require('../controllers/api/symptomApiController');
+const reminderApi = require('../controllers/api/reminderApiController');
+const userModel = require('../models/userModel');
+const pregnancyModel = require('../models/pregnancyModel');
 
