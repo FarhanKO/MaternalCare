0edd30e601c1477dd20054b9c76b1717798204c7
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 20:05:45 +0600

    Update dashboardController.js

--- App/controllers/dashboardController.js
+++ App/controllers/dashboardController.js
@@ -1,1 +1,8 @@
+const userModel = require('../models/userModel');
+const pregnancyModel = require('../models/pregnancyModel');
+const vitalModel = require('../models/vitalModel');
+const childModel = require('../models/childModel');
+const vaccinationModel = require('../models/vaccinationModel');
+const appointmentModel = require('../models/appointmentModel');
+const riskModel = require('../models/riskModel');
 
