8ff4d202f2bcb460662ec16183f4e9e938482d03
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-05 20:04:57 +0600

    Update emergencyController.js

--- App/controllers/emergencyController.js
+++ App/controllers/emergencyController.js
@@ -1,1 +1,11 @@
+const userModel = require('../models/userModel');
+const contentModel = require('../models/contentModel');
 
+exports.index = (req, res) => {
+  const user = userModel.current();
+  res.render('emergency', {
+    page: 'emergency', user,
+    contacts: userModel.emergencyContacts(user.id),
+    hospitals: contentModel.hospitals(),
+  });
+};
