b7d04cd0098be4f6f3f1bfe3b23079d4c8947b7a
MD Farhan <122352600+FarhanKO@users.noreply.github.com>
2026-08-07 20:55:56 +0600

    Update App.tsx

--- App/frontend/src/App.tsx
+++ App/frontend/src/App.tsx
@@ -1,1 +1,5 @@
+import { Suspense, lazy } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { Background } from '@/components/landing/Background';
+import { PageLoader } from '@/components/ui/Loader';
 
